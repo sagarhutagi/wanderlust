7f0769f4a1ac2262eb616c18cc75c4e3abaae732
sagarhutagi <sagarmh6364@gmail.com>
2025-10-10 14:14:43 +0530

    New Listings feature

--- app.js
+++ app.js
@@ -5,49 +5,74 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 
 app.set("view engine", "ejs");
-app. set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 
 const port = 8080;
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust"
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
-main().then(()=>{
+main()
+  .then(() => {
     console.log("The Database is Connected");
-}).catch((err)=>{
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
 async function main() {
-    await mongoose.connect(MONGO_URL)
+  await mongoose.connect(MONGO_URL);
 }
 
-app.listen(port, ()=>{
-    console.log(`The website is listening to localhost:${port}`);  
-})
+// app.get("/testListing", async (req,res)=>{
 
-app.get("/",(req,res)=>{
-    res.send("This is Home Page")
-})
+//     const sampleListing = new Listing({
+//         title: "Sagar's Home",
+//         description: "Nothing Much",
+//         price: "12,000",
+//         location: "Hubli, Karnataka",
+//         country: "India"
+//     })
 
-app.get("/testListing", async (req,res)=>{
-    
-    const sampleListing = new Listing({
-        title: "Sagar's Home", 
-        description: "Nothing Much",
-        price: "12,000",
-        location: "Hubli, Karnataka", 
-        country: "India"
-    })
+//     await sampleListing.insertMany().then(()=>{
+//         console.log("Sample Data Saved");
+//         res.send("Working")
+//     }).catch((e)=>{
+//         console.log(e);
+//         res.send("Not Working")
+//     });
+// })
 
-    await sampleListing.saveMany().then(()=>{
-        console.log("Sample Data Saved");
-        res.send("Working")
-    }).catch((e)=>{
-        console.log(e);
-        res.send("Not Working")
-    });
-})
+app.listen(port, () => {
+  console.log(`The website is listening to http://localhost:${port}`);
+});
 
-app.get("/listings", async (req, res)=>{
-    const allListings = await Listing.find({});
-    res.render("listings/index.ejs", {allListings})
-})
+app.get("/", (req, res) => {
+  res.send("This is Home Page");
+});
+
+// Index Route
+app.get("/listings", async (req, res) => {
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
+});
+
+// New Route
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs");
+});
+
+// New Route Post
+
+app.post("/listings", (req, res) => {
+  const body = new Listing(req.body);
+
+  body.save();
+  res.redirect("/listings");
+});
+
+// Show Route
+app.get("/listings/:id", async (req, res) => {
+  let { id } = req.params;
+  let listing = await Listing.findById(id);
+  res.render("listings/show.ejs", { listing });
+});
--- models/listing.js
+++ models/listing.js
@@ -16,7 +16,7 @@ const listingSchema = new Schema({
         ? "https://plus.unsplash.com/premium_photo-1759807887811-ef112449a724?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         : v,
   },
-  price: String,
+  price: Number,
   location: String,
   country: String,
 });
